41301c765c563dffe88d5cbaa1a3a41aff664e23
Sergio Tocalini Joerg <stocal1@Sergios-MacBook-Pro.local>
2018-02-19 14:59:01 +0100

    Fixing deploy_zabbix

--- deploy_zabbix.sh
+++ deploy_zabbix.sh
@@ -3,6 +3,7 @@ SOURCE_DIR=$(dirname $0)
 ZABBIX_DIR=/etc/zabbix
 
 mkdir -p ${ZABBIX_DIR}/scripts/agentd/habbixy
+cp ${SOURCE_DIR}/habbixy/map.index ${ZABBIX_DIR}/scripts/agentd/habbixy/
 cp ${SOURCE_DIR}/habbixy/habbixy.conf.example ${ZABBIX_DIR}/scripts/agentd/habbixy/
 cp ${SOURCE_DIR}/habbixy/habbixy.sh ${ZABBIX_DIR}/scripts/agentd/habbixy/
 cp ${SOURCE_DIR}/habbixy/zabbix_agentd.conf ${ZABBIX_DIR}/zabbix_agentd.d/habbixy.conf
